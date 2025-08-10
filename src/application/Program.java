a9284d8aaec27078c0e06f8e4e9ceae727e9536e
Yuri Lage <slageyuri@gmail.com>
2025-08-10 01:47:49 -0300

    Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
 import Entities.Department;
+import Entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj );
 
-        System.out.println(obj);
+
+        System.out.println(seller);
     }
 }
