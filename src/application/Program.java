5b35d38d5e10f0879cbbf4693ee620452f6c35bb
Yuri Lage <slageyuri@gmail.com>
2025-08-10 01:41:07 -0300

    Department

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import Entities.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+
+        System.out.println(obj);
+    }
+}
